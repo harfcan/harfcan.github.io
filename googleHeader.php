50a7af81ec19de8f48b11e5ed7f1ad62ce658da2
harfcan <harfcanmail@gmail.com>
2025-05-10 22:52:55 -0400

    Add files via upload

---
+++ googleHeader.php
@@ -0,0 +1,11 @@
+<head>
+  <title>My Website</title>
+  <!-- Google Analytics Tag -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECTE4D9MK9"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-ECTE4D9MK9');
+  </script>
+</head>
